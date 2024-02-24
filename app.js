fc55e795de8049828d0a610c958466b54edd10eb
Prateek Joshi <prateekjoshi0123@gmail.com>
2024-02-24 20:20:18 +0530

    using sql queries to fetch prods from db

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const errorController = require('./controllers/error');
+
+const db = require('./util/database')
 
 const app = express();
 
@@ -12,6 +15,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
